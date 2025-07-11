5744b7b1dba8ce227638f51315ff2a7ca165316c
Kyrd0x <61880638+Kyrd0x@users.noreply.github.com>
2025-07-11 13:21:15 +0200

    Update win10_setup.ps1

--- scripts/win10_setup.ps1
+++ scripts/win10_setup.ps1
@@ -5,6 +5,13 @@
 # Install : Python 32bits 3.12.4 + upgrade pip + install Pillow / pywintrace
 # Create Ninite from selection of software
 
+# Summary
+# Context : Windows 10 fresh install, NETWORK WORKING
+# Pre checks : admin rights, network connection OK
+# Disable : Defender / Firewall / Updates
+# Install : Python 32bits 3.12.4 + upgrade pip + install Pillow / pywintrace
+# Create Ninite from selection of software
+
 # Prerequis :
 # Permissions admin (a tester)
 # Acces internet (a tester)
